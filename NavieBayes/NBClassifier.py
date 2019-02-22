d1bfa5ed51e24a6264e72100a841d49e7eac8c8e
jessiedong1 <zdong1@cub.uca.edu>
2019-02-21 23:49:29 -0600

    Update the dataset

--- NavieBayes/NBClassifier.py
+++ NavieBayes/NBClassifier.py
@@ -1,16 +1,19 @@
-
 import pandas as pd
 import random as rd
 import math
 
 def main():
-    filename ='D:\Spring2019\DataMining\LabelTest.csv'
+    filename ='D:\Spring2019\DataMining\Dataset\Output.csv'
     rd.seed(1)
     #print(filename)
+
     dataset = loadCSV(filename)
     #print(dataset.iat[0,0])
     atts = dataset.columns.values
-    atts = atts[1:((len(atts))-1)]
+    atts = atts[8:((len(atts))-1)]
+
+
+
     #print(dataset['GRAPEFRUIT PEEL'])
     #print(atts)
     #print(dataset.values)
@@ -30,7 +33,7 @@ def main():
     dataset_Y_pro = Cal_All_Attributs(dataset_Y,atts)
     dataset_N_pro = Cal_All_Attributs(dataset_N,atts)
     #print(dataset_Y_pro)
-    #print(dataset_N_pro)
+    print(dataset_N_pro)
 
 
 """
@@ -40,12 +43,14 @@ def main():
     df = pd.DataFrame(array,labels)
 """
 
+#Load CSV File
 def loadCSV(filename):
     dataset = pd.read_csv(filename)
     dataset.head()
     dataset = pd.DataFrame(dataset)
     return dataset
 
+#K-fold
 def Cross_Validation(dataset, n_splits):
     num_instances = dataset.shape[0]
     train_set = pd.DataFrame()
@@ -57,11 +62,13 @@ def Cross_Validation(dataset, n_splits):
 
     return [train_set, dataset_copy]
 
+#Seperate Class by 'Label'
 def Group_Class(dataset):
     class_N, class_Y = (g for _, g in dataset.groupby('Label'))
     #class_N, class_Y = dataset.groupby('Label')
     return [class_Y, class_N]
 
+#Calculate the probablity of one attributs in one class
 def Cal_Pro(class_Y,att):
     #print(class_Y)
     #att_0, att_1= (g for _, g in class_Y.groupby(att))
@@ -78,12 +85,26 @@ def Cal_Pro(class_Y,att):
     pro_1 = float(att_1/(len(class_Y)))
     return [pro_0,pro_1]
 
+#Calculate the probabblity of all the attributes in the same class
+#Can only calculate one class each time
 def Cal_All_Attributs(class_Y, atts):
     array = {atts[i]: [Cal_Pro(class_Y,(atts[i]))]
                  for i in range(len(atts))}
     labels = ['ATT=0', 'ATT=1']
     ar = pd.DataFrame(array,labels)
     return ar
+#Based on training set, calculate the bayesian
+def Get_Pro(dataset,dataset_Y_Pro):
+
+    for i in range(dataset.shape[0]):
+        for j in range(dataset.shape[1]):
+            if(dataset.iat[i,j]==0):
+                print()
+
+
+
+
+
 
 
 
